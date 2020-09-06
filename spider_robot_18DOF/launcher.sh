3a8d4dda2d9e806709a53e935a8948acd35f13c7
bejec <bejecreimer@gmail.com>
2020-09-06 14:27:43 -0700

    add launcher at bootup to open example GUI

---
+++ spider_robot_18DOF/launcher.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# launcher.sh
+# navigate to home directory, then to this directory, then execute python script, then back home
+# https://medium.com/@arslion/starting-python-gui-program-on-raspberry-pi-startup-56fb4e451cc1
+# Need to create launcher.desktop file in ~/.config/autostart and fill it with:
+# [Desktop Entry]
+# Name=spiderController
+# Type=Application
+# Comment=runs spider Controller
+# Exec=/usr/bin/python /home/pi/Projects/spider-robot-18DOF/spider_robot_18DOF/spiderController.py
+# then to make it executable
+# chmod +x /home/pi/.config/autostart/launcher.desktop
+cd /
+cd home/pi/Projects/spider-robot-18DOF/spider_robot_18DOF
+sudo python spiderController.py
+cd /
+
