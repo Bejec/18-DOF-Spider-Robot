02f6b9ce1d64bbf33e88d04e7aac00b31a1a5f38
bejec <bejecreimer@gmail.com>
2020-09-06 00:10:02 -0700

    add contents for setup.py and ran setuptools

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='spiderRobot',
+	version='0.1',
+	description='module to interface with hardware controls on robot platform',
+	author='Bejec',
+	author_email='bejecreimer@gmail.com',
+	packages=find_packages(),
+)
