92460c393502ea913e9b30af4247a3520d961de9
bejec <bejecreimer@gmail.com>
2020-09-17 04:45:25 +0100

    update launcher.sh's with instructions to how to setup crontab to schdule python file execution at startup

--- spider_robot_18DOF/launcher.sh
+++ spider_robot_18DOF/launcher.sh
@@ -10,6 +10,13 @@
 # Exec=/usr/bin/python3 /home/pi/Projects/spider-robot-18DOF/spider_robot_18DOF/spiderController.py
 # then to make it executable
 # chmod +x /home/pi/.config/autostart/launcher.desktop
+# mkdir /logs
+# run sudo crontab -e
+# @reboot sh /home/pi/Projects/spider-robot-18DOF/spider_robot_18DOF/launcher.sh >/home/pi/logs/cronlog 2>&1
+# sudo reboot
+# confirm that cat /logs/cronlog has spiderRobot.py python return verbose
+
+
 touch /dev/shm/ctr
 touch /dev/shm/rtc
 touch /dev/shm/vid
--- spider_robot_18DOF/launcherRobot.sh
+++ spider_robot_18DOF/launcherRobot.sh
@@ -10,6 +10,12 @@
 # Exec=/usr/bin/python /home/pi/Projects/spider-robot-18DOF/spider_robot_18DOF/spiderController.py
 # then to make it executable
 # chmod +x /home/pi/.config/autostart/launcher.desktop
+# mkdir /home/pi/logs
+# run sudo crontab -e
+# add line in file and save
+# @reboot sh /home/pi/Projects/spider-robot-18DOF/spider_robot_18DOF/launcherRobot.sh >/home/pi/logs/cronlog 2>&1
+# sudo reboot and confirm that spiderRobot.py verbose shows up in cat /logs/cronlog
+
 touch /dev/shm/ctr
 touch /dev/shm/rtc
 touch /dev/shm/vid
