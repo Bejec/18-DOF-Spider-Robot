dbe025fdd3d84c8a6c93f22f695f5cc9f830bf6c
bejec <bejecreimer@gmail.com>
2020-09-17 04:24:20 +0100

    update launcherRobot.sh, also setup robot to launch spiderRobot.py at startup

--- spider_robot_18DOF/launcherRobot.sh
+++ spider_robot_18DOF/launcherRobot.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # launcherRobot.sh
-navigate to home directory, then to this directory, then execute python script, then back home
+# navigate to home directory, then to this directory, then execute python script, then back home
 # https://medium.com/@arslion/starting-python-gui-program-on-raspberry-pi-startup-56fb4e451cc1
 # Need to create launcher.desktop file in ~/.config/autostart and fill it with:
 # [Desktop Entry]
@@ -10,7 +10,10 @@ navigate to home directory, then to this directory, then execute python script, 
 # Exec=/usr/bin/python /home/pi/Projects/spider-robot-18DOF/spider_robot_18DOF/spiderController.py
 # then to make it executable
 # chmod +x /home/pi/.config/autostart/launcher.desktop
+touch /dev/tmp/ctr
+touch /dev/tmp/rtc
+touch /dev/tmp/vid
 cd /
 cd home/pi/Projects/spider-robot-18DOF/spider_robot_18DOF
-sudo python spiderRobot.py
+sudo python3 spiderRobot.py
 cd /
